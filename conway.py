4132cb52b52583d7873b0a07ad3d2bea14794c6b
nilskingston <nilskingston@hanovernorwichschools.org>
2015-10-21 13:57:26 -0400

    Updated from Brython Server: 10/21/2015 1:57:25 PM

--- conway.py
+++ conway.py
@@ -1,8 +1,27 @@
 """
 conway.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Nils Kingston   
+Credit: Roger
 Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+# Background
+
+class SpaceGame(App):
+    """
+    Tutorial4 space game example.
+    """
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        black = Color(0, 1)
+        noline = LineStyle(0, black)
+        bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
+        bg = Sprite(bg_asset, (0,0))
+myapp = SpaceGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+myapp.run()
